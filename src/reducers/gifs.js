4d7d52b041ccb0af10ae396bddb2a3e6972c56fd
github username <github email address>
2019-11-20 16:05:40 -0500

    add a reducers directory returning the default state

---
+++ src/reducers/gifs.js
@@ -0,0 +1,17 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+// an empty version of what ever this
+// paticular reducer is responsable for
+// returning
+
+// at what point in time will we use the default?
+// When we initiate
+
+// When @@INIT gets fired it will return the
+//.. default state of all of your reducers
+
+// default = name what ever I want in index.js
