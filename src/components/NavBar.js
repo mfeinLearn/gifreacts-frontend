fd68902c1ef15ff645ebb251a1d45b4184c3965c
github username <github email address>
2019-11-20 16:05:11 -0500

    add a components folder with a NavBar and SearchBar with a onFormSubmit method

---
+++ src/components/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { connect } from 'react-redux'
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+        <span role="img" aria-labelledby="myNameId">😖 😞 😟 😤 😢 😭 😦 😧 😨 😩</span>
+       <h2>Welcome to Gif React!</h2>
+        <span role="img" aria-labelledby="myNameId">😀 😁 😂 😊 😋 😎 😍 😑 😶 🙄</span>
+    </div>
+  )
+}
+
+
+// const mapStateToProps = ({  }) => {
+//   return {
+//
+//   }
+// }
+
+export default NavBar;
