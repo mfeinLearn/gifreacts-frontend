6e7e9b0246aeabc3f408c145d969d4adf4d2f79e
github username <github email address>
2019-11-20 16:02:34 -0500

    moved the store and all of it’s dependencies into store.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import store from './store.js'
 
- const gifs = () => []
- const reducer = combineReducers({
-   gifs
- })
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // returns a function
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(<Provider store={  store }><App /></Provider>, document.getElementById('root'));
 
