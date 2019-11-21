a9f87109799bb4d5e485535dccfa094a47c05b10
github username <github email address>
2019-11-21 10:22:40 -0500

    add setGif action

--- src/actions/gifs.js
+++ src/actions/gifs.js
@@ -1,8 +1,32 @@
 // synchronous action creators
 
-export const setCurrentUser = user => {
+export const setGifs = gifs => {
   return {
-    type: "SET_CURRENT_USER",
-    user
+    type: "SET_GIFS",
+    gifs
   }
 }
+// do last
+
+// VVV testing something
+// export const getMyTrips = () => {
+//   return dispatch => {
+//     return fetch("http://localhost:3001/api/v1/trips", {
+//       credentials: "include",
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//     })
+//     .then(r => r.json())
+//     .then(response => {
+//       if (response.error) {
+//         alert(response.error)
+//       } else {
+//         // console.log(response.data)
+//         dispatch(setMyTrips(response.data))
+//       }
+//     })
+//     .catch(console.log)
+//   }
+// }
