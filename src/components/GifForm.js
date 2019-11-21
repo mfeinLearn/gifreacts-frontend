fef4035d05591b9f0609618e5c82c433fcb52eec
github username <github email address>
2019-11-21 13:59:30 -0500

    add form data and gif url to local state

--- src/components/GifForm.js
+++ src/components/GifForm.js
@@ -1,18 +1,53 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { addGif } from "../actions/gifs.js"
+//import { connect } from 'react-redux'
+//import { addGif } from "../actions/gifs.js"
 
-const GifForm = ({addGif}) => {
+class GifForm extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  state = {
+    emotion: '',
+    humer_type: '',
+    aGif: this.props.gif.images.downsized.url
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    console.log(this.state.aGif)
+    // console.log(this.state.humer_type)
+
+
+    //this.props.onSubmit(this.state.term);
+    // console.log(this.state.term)
+  }
+
+  render() {
   return (
-    <form onSubmit={addGif}>
-      <input placeholder="emotion" name="emotion" type="text" />
-      <input placeholder="humer type" name="humer type" type="text" />
+    <form onSubmit={this.onFormSubmit}>
+      <input
+        placeholder="emotion"
+        name="emotion"
+        type="text"
+        value={this.state.emotion}
+        onChange={(e) => this.setState({ emotion: e.target.value })}
+      />
+      <input
+        placeholder="humer type"
+        name="humer type"
+        type="text"
+        value={this.state.humer_type}
+        onChange={(e) => this.setState({ humer_type: e.target.value })}
+      />
       <input type="submit" value="Submit" />
     </form>
-  )
+    )
+  }
 }
 
-export default connect(null, { addGif })(GifForm)
+export default GifForm;
 
 // I still need....
 
@@ -21,3 +56,15 @@ export default connect(null, { addGif })(GifForm)
 
 // 2. another case statment
 //.. in my currentUser reducer
+
+////////
+// <form onSubmit={this.onFormSubmit} className="ui form">
+//   <div className="field">
+//   <label>Gif Search</label>
+//   <input
+//     type="text"
+//     value={this.state.term}
+//     onChange={(e) => this.setState({ term: e.target.value })}
+//   /> {/*call at some point in time in the future*/}
+//   </div>
+// </form>
