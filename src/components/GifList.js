8e5c88c763e8d264daa22cf590b57ebd7b187bfb
github username <github email address>
2019-11-22 17:30:34 -0500

    add changes to get gifs from redux store and display each gif

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import GifCard from './GifCard'
+import { connect } from 'react-redux'
 
 const GifList = props => {
   // console.log(props.gif[0])
-  const gifs = props.gif.map((gif) => {
-    return <GifCard key={gif.id} gif={gif} />
+  const gifs = props.gifs.map((gif) => {
+    return <img src={gif.name} />
   });
 
   return gifs;
 };
 
-export default GifList;
+const mapStateToProps = (state) => {
+  return {gifs: state.gifs}
+}
+
+export default connect(mapStateToProps)(GifList);
