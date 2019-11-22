f3b4840882e5f18594f33b9ebf64aeeba5388519
github username <github email address>
2019-11-22 17:32:12 -0500

    add changes to return the original state along with the new gif

--- src/reducers/gifs.js
+++ src/reducers/gifs.js
@@ -1,7 +1,7 @@
 export default (state = [], action) => {
   switch (action.type) {
     case "SET_GIF":
-      //return action.gif
+      return [...state, action.gif]
     default:
       return state
   }
