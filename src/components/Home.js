1c06bc6b9d48a94ab9a1e3066f2198dcca5325d9
github username <github email address>
2019-11-24 09:34:37 -0500

    add Home Component

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+//NavLink - are great for nav bars when
+//.. you are going to see those links all times
+// Link - is a one time thing
+const Home = () => {
+  return (
+    <div className="NavBar">
+        <span role="img" aria-labelledby="myNameId">😖 😞 😟 😤 😢 😭 😦 😧 😨 😩</span>
+       <h2>Welcome to Gif React!</h2>
+        <span role="img" aria-labelledby="myNameId">😀 😁 😂 😊 😋 😎 😍 😑 😶 🙄</span>
+    </div>
+  )
+}
+
+export default Home;
