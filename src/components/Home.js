e9c0d34e1e95c627ac866ffb5bcbeebcd731ba89
github username <github email address>
2019-11-25 10:48:20 -0500

    add comment to Link component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
+
 //NavLink - are great for nav bars when
 //.. you are going to see those links all times
 // Link - is a one time thing
