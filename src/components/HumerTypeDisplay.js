ee6875628f11906928f098a610fadc7e04ab38c4
github username <github email address>
2019-11-27 09:34:24 -0500

    add the HumerTypeDisplay card functionality

---
+++ src/components/HumerTypeDisplay.js
@@ -0,0 +1,43 @@
+import './HumerTypeDisplay.css'
+import React from 'react';
+
+const humerConfig = {
+  ok: {
+    text: "It's alright",
+    iconName: "hand spock icon"
+  },
+  funny: {
+    text: "WOW LOL!",
+    iconName: "thumbs up icon"
+  },
+  rotfl: {
+    text: "rolling on the floor laughing!",
+    iconName: "thumbs up icon"
+  }
+};
+
+ const getHumer = (rating) => {
+   if (rating <= 1) {
+     return "ok"
+   }
+   else if (rating >= 2 && rating <= 3) {
+     return "funny"
+   } else {
+     return "rotfl"
+   }
+ };
+
+const HumerTypeDisplay = (props) => {
+  const humer = getHumer(props.humer_rating);
+  const { text, iconName } = humerConfig[humer];
+
+  return (
+  <div className={`humer-type-display ${humer}`}>
+    <i className={`icon-left large ${iconName} icon`} />
+    <h1>{text}</h1>
+    <i className={`icon-right large ${iconName} icon`} />
+  </div>
+  );
+};
+
+export default HumerTypeDisplay;
