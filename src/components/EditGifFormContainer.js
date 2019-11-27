31514df3ea17781e67b0ec715d2c8340a7c9df6a
github username <github email address>
2019-11-27 09:27:56 -0500

    testing if I am able to hit this component

---
+++ src/components/EditGifFormContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from 'react-router-dom'
+//NavLink - are great for nav bars when
+//.. you are going to see those links all times
+// Link - is a one time thing
+const EditGifFormContainer = () => {
+  return (
+    <div className="NavBar">
+        <span role="img" aria-labelledby="myNameId">😖 </span>
+       <h2>This is EditGifFormContainer</h2>
+        <span role="img" aria-labelledby="myNameId"> 🙄</span>
+    </div>
+  )
+}
+
+export default EditGifFormContainer;
