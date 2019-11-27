54e9d3f9e9fd4235dc1185069c8f14059bb97663
github username <github email address>
2019-11-27 09:26:58 -0500

    add changes to connect the component and import and implement more components - connect, withRouter and mapStateToProps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux'
 // import axios from './api/axios.js'
 // import { setGifResponseData } from './actions/gifs.js'
 import NavBar from './components/NavBar.js'
@@ -11,8 +11,11 @@ import Home from './components/Home.js'
 import Blank from './components/Blank.js'
 import AllGifs from './components/AllGifs.js'
 import NewGifFormContainer from './components/NewGifFormContainer.js'
+import EditGifFormContainer from './components/EditGifFormContainer.js'
+import GifShowCard from './components/GifShowCard.js'
 import {Switch} from 'react-router-dom'
-import { BrowserRouter as Router,Route} from 'react-router-dom'
+import { BrowserRouter as Router,Route, withRouter} from 'react-router-dom'
+
 
 
 class App extends React.Component {
@@ -42,14 +45,20 @@ class App extends React.Component {
       // <GifList gif={this.state.gif}/>
       // <GifList />
       */}
+      {console.log("this.props.gifs:",this.props.gifs)}
           <Router>
             <NavBar/>
             <Switch>
             <Route exact path='/home' component={Home}/>
             <Route exact path='/gifs' component={AllGifs}/>
             <Route exact path='/gifs/new' component={NewGifFormContainer}/>
-          {/*   // <Route exact path='/gifs/:id' render={}/> */}
-           {/*<Route exact path='/gifs/:id/edit' component={EditGifFormContainer}/> */}
+             <Route exact path='/gifs/:id' render={ props  => {
+               const gif = this.props.gifs.find(gif => gif.id === props.match.params.id)
+               console.log(this.props.gifs)
+               return <GifShowCard gif={gif} {...props}/>
+               }
+             }/>
+            <Route exact path='/gifs/:id/edit' component={EditGifFormContainer}/>
             <Route exact path='/blank' component={Blank}/>
             </Switch>
           </Router>
@@ -60,8 +69,14 @@ class App extends React.Component {
   }
 }
 
-export default App;
-//export default connect(null, {setGifResponseData})(App);
+const mapStateToProps = state => {
+  return ({
+    gifs: state.gifs
+  })
+}
+
+//export default App;
+export default withRouter(connect(mapStateToProps)(App));
 
 // let APIKEY = ""
 // let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
