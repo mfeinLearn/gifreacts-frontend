489988898e5f95455f1c4295c73b9825b0acf367
github username <github email address>
2019-12-02 01:36:01 -0500

    add changes to convert functional component to class component and used action creator when component mounts

--- src/components/Blank.js
+++ src/components/Blank.js
@@ -1,16 +1,35 @@
 import React from 'react';
-// import { Link } from 'react-router-dom'
-//NavLink - are great for nav bars when
-//.. you are going to see those links all times
-// Link - is a one time thing
-const Blank = () => {
-  return (
-    <div className="NavBar">
-        <span role="img" aria-labelledby="myNameId">😖 </span>
-       <h2>This is Blank</h2>
-        <span role="img" aria-labelledby="myNameId"> 🙄</span>
-    </div>
-  )
+import { connect } from 'react-redux'
+import { fetchGifs } from '../actions/gifs'
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
+class Blank extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchGifs()
+  }
+
+  render(props) {
+      return (
+
+        <div className="NavBar">
+            <span role="img" aria-labelledby="myNameId">😖 </span>
+           <h2>This is Blank</h2>
+{/*  //          {this.props.gifs.map(gif => */}
+{/*  // <li key={gif.id}> {gif.attributes.name} </li> )} */}
+
+            <span role="img" aria-labelledby="myNameId"> 🙄</span>
+        </div>
+      )
+  }
 }
 
-export default Blank;
+const mapStateToProps = state => {
+  return {
+    gifs: state.gifs
+  }
+}
+
+export default connect(mapStateToProps, {fetchGifs})(Blank)
+/////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
