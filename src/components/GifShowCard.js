3b9a3e0b670bc533e4045eeef4b9ecc0f45fce7b
github username <github email address>
2019-12-03 18:34:20 -0500

    add changes to import and use the fetchGifs action creator

--- src/components/GifShowCard.js
+++ src/components/GifShowCard.js
@@ -1,21 +1,33 @@
-// import React from 'react';
-// //NavLink - are great for nav bars when
-// //.. you are going to see those links all times
-// // Link - is a one time thing
-// const GifShowCard = ({gif}) => {
-// console.log("hi")
-// console.log(gif)
-// console.log("hi")
-//   return (
-//     gif ?
-//     <div>
-//       <h3>{gif.emotion.attributes.name}</h3>
-//       {console.log("hi")}
-//       {console.log(gif)}
-//       {console.log("hi")}
-//     </div> :
-//     <p>This the the GifShowCard with no gif!</p>
-//   )
-// }
-//
-// export default GifShowCard;
+import React from 'react';
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom';
+import { fetchGifs } from '../actions/gifs'
+
+//NavLink - are great for nav bars when
+//.. you are going to see those links all times
+// Link - is a one time thing
+
+class GifShowCard extends React.Component {
+  componentDidMount() {
+    this.props.fetchGifs()
+  }
+
+  render(props) {
+  return (
+    // gif ?
+    // <div>
+    //   <h3>{gif.attributes.name}</h3>
+    // </div> :
+    // <p>This the the GifShowCard with no gif!</p>
+    0
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    gifs: state.gifs
+  }
+}
+
+export default withRouter(connect(mapStateToProps, {fetchGifs})(GifShowCard));
