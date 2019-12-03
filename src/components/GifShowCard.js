0a72ed1622ed7bfd674cdaa61498450a878aaa7a
github username <github email address>
2019-12-03 12:14:54 -0500

    add comments to entire file

--- src/components/GifShowCard.js
+++ src/components/GifShowCard.js
@@ -1,21 +1,21 @@
-import React from 'react';
-//NavLink - are great for nav bars when
-//.. you are going to see those links all times
-// Link - is a one time thing
-const GifShowCard = ({gif}) => {
-console.log("hi")
-console.log(gif)
-console.log("hi")
-  return (
-    gif ?
-    <div>
-      <h3>{gif.emotion.attributes.name}</h3>
-      {console.log("hi")}
-      {console.log(gif)}
-      {console.log("hi")}
-    </div> :
-    <p>This the the GifShowCard with no gif!</p>
-  )
-}
-
-export default GifShowCard;
+// import React from 'react';
+// //NavLink - are great for nav bars when
+// //.. you are going to see those links all times
+// // Link - is a one time thing
+// const GifShowCard = ({gif}) => {
+// console.log("hi")
+// console.log(gif)
+// console.log("hi")
+//   return (
+//     gif ?
+//     <div>
+//       <h3>{gif.emotion.attributes.name}</h3>
+//       {console.log("hi")}
+//       {console.log(gif)}
+//       {console.log("hi")}
+//     </div> :
+//     <p>This the the GifShowCard with no gif!</p>
+//   )
+// }
+//
+// export default GifShowCard;
