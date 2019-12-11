c7dfe23d478428c234c4de058bb39be25684cc6c
github username <github email address>
2019-12-11 16:53:57 -0500

    add changes to delete the fetchGifs action creator and added editGifReaction action creator and add event handlers and form with associated gif

--- src/components/GifShowCard.js
+++ src/components/GifShowCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom';
-import { fetchGifs } from '../actions/gifs'
+import { editGifReaction } from '../actions/gifs'
 
 //NavLink - are great for nav bars when
 //.. you are going to see those links all times
@@ -12,13 +12,40 @@ class GifShowCard extends React.Component {
   //   this.props.fetchGifs()
   // }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleEditClick = (event) => {
+    event.preventDefault()
+      const gif = this.props.gif;
+      console.log("this.props.gif in GifShowCard's handleEditClick",gif)
+     let singleGifReaction = {gif, id: gif.id} // did not do yet
+     this.props.editGifReaction(singleGifReaction)// did not create yet
+  }
+
   render() {
+    //console.log("this.props.gif in GifShowCard",this.props.gif)
   return (
     <div>
-
     { this.props.gif ?
-      // img tag && new form 
-      <h3>{this.props.gif.attributes.name}</h3>
+  <form onSubmit={this.handleEditClick}>
+  // img tag && new form
+    <img
+      height="200" width="200"
+      alt={this.props.gif.attributes.name}
+      src={this.props.gif.attributes.name}
+    />
+    <br />
+      <label>emotion name: </label>
+      <input type='text' placeholder='emotion name' value={this.props.gif.attributes.emotion_name} name="emotion_name" onChange={this.handleChange}/><br/>
+      <label>humer type range: </label>
+      <input type='text' placeholder='humer type range' value={this.props.gif.attributes.humer_type_range} name="humer_type_range" onChange={this.handleChange}/><br/>
+      <input type="submit"/>
+  </form>
+
         :
     <p>This the the GifShowCard with no gif!</p> }
     </div>
@@ -32,4 +59,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, {fetchGifs})(GifShowCard));
+export default withRouter(connect(mapStateToProps, {editGifReaction})(GifShowCard));
