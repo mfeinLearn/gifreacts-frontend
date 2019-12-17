909c6a7999513429e0724843457285ffaee23763
github username <github email address>
2019-12-17 18:30:03 -0500

    add changes to pass the history prop to the GifCard.

--- src/components/SingleGif.js
+++ src/components/SingleGif.js
@@ -4,7 +4,7 @@ import GifCard from './GifCard'
 const SingleGif = props => {
   // console.log(props.gif[0])
   const gifs = props.gif.map((gif) => {
-    return <GifCard key={gif.id} gif={gif} />
+    return <GifCard key={gif.id} gif={gif} history={props.history} />
   });
 
   return gifs;
