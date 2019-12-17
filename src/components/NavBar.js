0918baab73c3f5e6040f942757d95e1f27a30e1a
github username <github email address>
2019-12-17 18:29:42 -0500

    add changes to improve the css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div className="NavBar">
-      <NavLink exact activeClass to="/home">Home |  </NavLink>
-      <NavLink exact activeClass to="/gifs">All Gifs |  </NavLink>
-      <NavLink exact activeClass to="/gifs/new">New Gif  </NavLink>
+    <div className="ui three item menu">
+      <NavLink exact activeClass to="/home" className="item">Home</NavLink>
+      <NavLink exact activeClass to="/gifs" className="item">All Gif Reactions</NavLink>
+      <NavLink exact activeClass to="/gifs/new" className="item">New Gif Reaction</NavLink>
       {/*<NavLink exact activeClass to="/blank">Blank </NavLink> */}
     </div>
   )
